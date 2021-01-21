c5b2df3cb099ccf570a809d4859f44bbd384c27b
bluesinger75 <bluesinger75@gmail.com>
2021-01-21 15:44:53 +0900

    Node.js - 파일 읽기 (파일을 이용해 본문 구현)

--- main.js
+++ main.js
@@ -7,14 +7,35 @@ var app = http.createServer(function (request, response) {
   var queryData = new URL(_url, 'http://' + request.headers.host + '/')
     .searchParams;
   var _id = queryData.get('id');
-  console.log(queryData);
   if (_url == '/') {
-    _url = '/index.html';
+    title = 'Welcome';
   }
   if (_url == '/favicon.ico') {
     return response.writeHead(404);
   }
   response.writeHead(200);
-  response.end(_id);
+  // data 폴더에서 파라메타 id값과 동일한 파일을 읽어 변수 ${description} 로 <p> 테그 값을 입력한다.
+  fs.readFile(`data/${_id}`, 'utf8', function (err, description) {
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+    </html>
+    `;
+    response.end(template);
+  });
 });
 app.listen(3000);
